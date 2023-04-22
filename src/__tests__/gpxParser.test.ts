d6d511839c7aa3e11ea25694f22a151ec3dfd94e
bovan <bovan@bovan.no>
2023-04-22 16:47:07 +0200

    added more metadata to output
    - added new GPX file since old had bad elevation

--- src/__tests__/gpxParser.test.ts
+++ src/__tests__/gpxParser.test.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { gpxParser } from "../gpxParser";
 
 const gpxDir = path.join(__dirname, "..", "..", "test_data");
-const fileName = "run.gpx";
+const fileName = "Olavsspranget.gpx";
 
 async function getTestGpx() {
     const data = await readFile(path.join(gpxDir, fileName), "utf8");
@@ -17,7 +17,15 @@ test("Parser should return geoJSON response from a GPX file", async () => {
 
     expect(typeof result).toBe("object");
     expect(result).toHaveProperty("type", "Feature");
+    expect(result.properties).toHaveProperty("name", "Olavsspranget");
+    expect(result.properties).toHaveProperty("time", "2022-06-23T17:51:47Z");
+    expect(result.properties).toHaveProperty("elevationMax", 190);
+    expect(result.properties).toHaveProperty("elevationNeg", 189);
+    expect(result.properties).toHaveProperty("elevationPos", 185);
+    expect(result.properties).toHaveProperty("distance", 5305);
+
     expect(result.geometry.type).toBe("LineString");
     const geometry = result.geometry as LineString;
     expect(geometry.coordinates.length).toBeGreaterThan(0);
+    expect(result.properties).toHaveProperty("name", "Olavsspranget");
 });
